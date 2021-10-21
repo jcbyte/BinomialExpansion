e36b2abd5d82fe33629ff3e73ec7d2eae04dec99
jcbyte <joelcutler108@gmail.com>
2021-10-21 19:35:40 +0100

    Inputs

--- BinomialExpansion/Source.cpp
+++ BinomialExpansion/Source.cpp
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <string>
+
+void SplitNumVar(std::string s, int* num, char** var)
+{
+	std::string numChars = "+-0123456789";
+
+	std::string numString;
+	std::string varString;
+	bool endOfNum = false;
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (endOfNum)
+		{
+			varString += s[i];
+		}
+		else
+		{
+			if (numChars.find(s[i]) != std::string::npos)
+			{
+				numString += s[i];
+			}
+			else
+			{
+				endOfNum = true;
+				varString += s[i];
+			}
+		}
+	}
+
+	*num = std::stoi(numString);
+
+	*var = new char[varString.length()];
+	for (int i = 0; i < varString.length(); i++)
+		(*var)[i] = varString[i];
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "Hello world" << std::endl;
+	std::cout << "(a+b)^n where n is an integer" << std::endl << std::endl;
+
+	std::string a;
+	std::cout << "a = "; std::cin >> a;
+	std::string b;
+	std::cout << "b = "; std::cin >> b;
+	std::string n;
+	std::cout << "n = "; std::cin >> n;
+
+	int aNum;
+	char* aVar = nullptr;
+	SplitNumVar(a, &aNum, &aVar);
+	int bNum;
+	char* bVar = nullptr;
+	SplitNumVar(b, &bNum, &bVar);
+	int nNum = std::stoi(n);
+
+
 
 	system("pause > nul");
 	return 0;
